4fa4c6d6d0cd8a98d461370f432e5c0ef679bd3f
Abhishek Saini <abhishek.saini@geminisolutions.in>
2020-03-23 18:32:36 +0530

    add | remove | get methods

--- src/MyArrayListAbhi.java
+++ src/MyArrayListAbhi.java
@@ -6,18 +6,21 @@ public class MyArrayListAbhi {
         int n = 5;
 
 //       ArrayList type Integer
-        ArrayList <Integer> myintarr = new ArrayList<Integer>(n);
+        ArrayList<Integer> myintarr = new ArrayList<Integer>(n);
 
 //      ArrayList:: add() method appends element.
-        for(int i = 0 ; i<n; i++)
-        myintarr.add(i+1);
+        for (int i = 0; i < n; i++)
+            myintarr.add(i + 1);
 
         System.out.println(myintarr);
-//remove element from at a particular index.
+//      remove element from at a particular index.
         myintarr.remove(2);
         System.out.println(myintarr);
         myintarr.remove(2);
         System.out.println("\n");
         System.out.println(myintarr);
+
+//      Get index of an element get()
+        System.out.println("Element at "+ 2 + " is " + myintarr.get(2));
     }
 };
