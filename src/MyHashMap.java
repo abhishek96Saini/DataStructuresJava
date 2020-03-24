71f86e8aaf381ca79a6ad23f5e8479713eeaa1bf
Abhishek Saini <abhishek.saini@geminisolutions.in>
2020-03-24 19:42:51 +0530

    i/o in HashMap

---
+++ src/MyHashMap.java
@@ -0,0 +1,26 @@
+import java.util.HashMap;
+
+public class MyHashMap {
+
+    public static void main(String[] args) {
+
+        HashMap<Integer, String> strIntHashMap = new HashMap();
+
+        System.out.println("Empty Hash map: "+strIntHashMap);
+
+//        insert (key, value) in hash map.
+        strIntHashMap.put(1,"Aarnav");
+        strIntHashMap.put(2,"Abhay");
+        strIntHashMap.put(3,"Akshat");
+
+        System.out.println(strIntHashMap);
+
+//        insert (key, value) only if not already existing
+        strIntHashMap.putIfAbsent(33,"Lakshay");;
+        strIntHashMap.putIfAbsent(33,"Lakshay");;
+        strIntHashMap.putIfAbsent(34,"Lakshay2");;
+
+        System.out.println(strIntHashMap);
+
+    }
+}
